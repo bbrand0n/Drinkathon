d91317cfcc9e0bcc0f87f176716d784562cbfbf1
bbrand0n <brandonginbons@icloud.com>
2024-05-02 13:13:24 -0400

    Initial Commit

---
+++ Drinkathon/DrinkathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkathonApp.swift
+//  Drinkathon
+//
+//  Created by Brandon Gibbons on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct DrinkathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
